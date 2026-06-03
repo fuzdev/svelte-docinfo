cf97b6e73917e7753fb51a0fc17c91e9beba3524
Ryan Atkinson <mail@ryanatkn.com>
2026-06-03 11:06:41 -0400

    wip

--- src/test/fixtures/svelte/types/intersection-union-html/input.svelte
+++ src/test/fixtures/svelte/types/intersection-union-html/input.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
 	import type {SvelteHTMLElements} from 'svelte/elements';
 
-	const {label, ...rest}: (SvelteHTMLElements['div'] | SvelteHTMLElements['span']) & {
+	const {
+		label,
+		...rest
+	}: (SvelteHTMLElements['div'] | SvelteHTMLElements['span']) & {
 		/** A label rendered inside the element. */
 		label: string;
 	} = $props();
